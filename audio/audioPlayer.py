e093ed025068418e991a27961c801ba3ac83002d
Dominik Mack <dominik.mack@icloud.com>
2022-10-01 16:05:26 +0200

    Refactored to use python modules, to match the new formatting requirements and removed python-vlc since it required to have vlc installed just to play sounds

--- audio/audioPlayer.py
+++ audio/audioPlayer.py
@@ -1,51 +1,53 @@
-import time, os, vlc, sys
-from mutagen.mp3 import MP3 
+import time, os, sys
+from os.path import join, dirname
 
-audio_path = os.getcwd() + "/display_audio"
+from mutagen.mp3 import MP3
+from playsound import playsound
+
+audio_path = join(dirname(dirname(__file__)), "display_audio")
 
 audio_config = None
 
-def runAudioPlayer(audioconfig, audiofiles):
+
+def run_audio_player(audioconfig, audiofiles):
     global audio_config
     audio_config = audioconfig
     time_end = time.time() * 60 * audio_config["repeat_duration"]
-    fileCount = 0
-    
-    if(len(audiofiles) == 0):
+    file_count = 0
+
+    if len(audiofiles) == 0:
         print("No Audio-Files loaded... Exiting Audio-Playback.")
         return None
-    
+
     while time.time() < time_end:
-        if(fileCount + 1 > len(audiofiles)):
-            fileCount = 0
-        
-        current_file_name = audiofiles[fileCount]
-        
+        if file_count + 1 > len(audiofiles):
+            file_count = 0
+
+        current_file_name = audiofiles[file_count]
+
         if " " in current_file_name:
-            current_file_name = renameAudiofile(audio_path, current_file_name)
-        
-        current_file_path = audio_path + "/" + current_file_name
-        
+            current_file_name = rename_audio_file(audio_path, current_file_name)
+
+        current_file_path = join(audio_path, current_file_name)
+
         print("Now Playing: " + current_file_name + "\r")
-        
-        player = vlc.MediaPlayer(current_file_path)
-        player.play()
-        
-        soundFile = MP3(current_file_path)
-        soundDuration = soundFile.info.length
-        
-        pauseAudioOutput(soundDuration)
-        
-        fileCount = fileCount + 1
-    
+
+        playsound(current_file_path)
+
+        sound_file = MP3(current_file_path)
+
+        file_count = file_count + 1
+
     print("Display-Mode finished... Exiting")
     sys.exit()
-    
-def pauseAudioOutput(currentSoundDuration):
-    time.sleep(currentSoundDuration)
+
+
+def parse_audio_output(current_sound_duration):
+    time.sleep(current_sound_duration)
     time.sleep(audio_config["audio_break"])
 
-def renameAudiofile(audio_path, filename):
-    newName = filename.replace(" ", "_")
-    os.rename(audio_path + "/" + filename, audio_path + "/" + newName)
-    return newName
+
+def rename_audio_file(audio_path, filename):
+    new_name = filename.replace(" ", "_")
+    os.rename(audio_path + "/" + filename, audio_path + "/" + new_name)
+    return new_name
